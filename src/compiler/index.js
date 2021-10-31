e516cf75ec9c072a3844b38dc8bfaedada76367b
胡磊 <hl@eteng.cn>
2021-10-31 11:11:19 +0800

    08.生成render函数

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -1,6 +1,6 @@
 // ast语法树：用对象来描述html语法的结构  虚拟dom：用对象来描述dom节点的结构
 import {parseHTML} from "./parser-html"
-
+const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/g; // 匹配{{}}里面的任意字符
 function genProps(attrs) { // 处理属性，拼接成属性的字符串
   // [{name: 'id', value: 'app'}] -> {id: app,a:1,b:2}
   let str = '';
@@ -20,10 +20,52 @@ function genProps(attrs) { // 处理属性，拼接成属性的字符串
   return `{${str.slice(0, -1)}}`;
 }
 
+function genChildren(el) {
+  let children = el.children;
+  if(children && children.length > 0) {
+    return `${children.map(c=>gen(c)).join(',')}`
+  } else {
+    return false
+  }
+}
+
+function gen(node) {
+  if(node.type === 1) {
+    // 元素标签
+    return generate(node)
+  } else {
+    // 文本
+    let text = node.text;
+    let tokens = [];
+    let match, index;
+    // 每次的偏移量 buffer.split()
+    let lastIndex = defaultTagRE.lastIndex = 0;
+    while(match = defaultTagRE.exec(text)) {
+      index = match.index;
+      if(index > lastIndex) {
+        tokens.push(JSON.stringify(text.slice(lastIndex, index)))
+      }
+      tokens.push(`_s(${match[1].trim()})`)
+      lastIndex = index + match[0].length;
+    }
+    if(lastIndex < text.length) {
+      tokens.push(JSON.stringify(text.slice(lastIndex)))
+    }
+    return `_v(${tokens.join('+')})`
+    
+    // a {{name}} b {{age}} c ===> _v("a"+_s(name)+"b"+_s(age)+"c")
+    return `_v(${text})`
+  }
+}
+
 function generate(el) {
+  let children = genChildren(el);
   let code = `_c("${el.tag}", ${
     el.attrs.length ? genProps(el.attrs) : 'undefined'
-  })
+  }${
+    children ? `,${children}` : ''
+  }
+  )
 
   `;
 
@@ -41,8 +83,9 @@ export function compileToFunction(template) {
   // 将ast树，再次转换成js的语法
   // _c("div", {id: app}, _c("p", undefined, -v('hello' + _s(name) )), _v('hello'))
   // _c render函数 _v创建文本 name可能是个对象，要使用JSON.stringify，_s转成字符串
-  console.log(code)
-  return function render() {
-    
-  }
+  // 所有的模板引擎实现 都需要new Function + with
+  // let renderFn = new Function();
+  let renderFn = new Function(`with(this) { return ${code}}`);
+  console.log(renderFn)
+  return renderFn
 } 
